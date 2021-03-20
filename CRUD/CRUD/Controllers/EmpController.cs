95cd56cb2c2f35c427761ea9922658857608ab1a
Adnan Raja <adnannrajaa@outlook.com>
2021-03-20 21:14:45 +0500

    fgyfyfyu

---
+++ CRUD/CRUD/Controllers/EmpController.cs
@@ -0,0 +1,69 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public EmpController(DatabaseContext context)
+        {
+            _context = context;
+        }
+       
+        [HttpGet]
+        public ActionResult<List<Employee>> Get()
+        {
+            return Ok(_context.Employee.ToList()); 
+                
+        }
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult<Employee> Get(int id)
+        {
+            
+             var employee=_context.Employee.FirstOrDefault(a => a.id == id);
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> Post(Employee employee)
+        {
+            _context.Employee.Add(employee);
+             _context.SaveChanges();
+            return Ok(employee);
+
+        }
+
+        [HttpPut]
+        public ActionResult<Employee> Put(Employee employee)
+        {
+           var employeeInDb = _context.Employee.FirstOrDefault(a => a.id == employee.id);
+            employeeInDb.name = employee.name;
+            employeeInDb.password = employee.password;
+            employeeInDb.email = employee.email;
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+        [Route("{id}")]
+        [HttpDelete]
+        public ActionResult<Employee> Delete(int id)
+        {
+            var employeeInDb = _context.Employee.FirstOrDefault(a => a.id ==id);
+            _context.Remove(employeeInDb);
+            _context.SaveChanges();
+            return Ok(employeeInDb);
+           
+        }
+    }
+
+    
+}
